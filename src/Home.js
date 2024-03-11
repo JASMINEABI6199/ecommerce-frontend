ad36806ddce51f53bc93fd0b0263d8cf8968aa12
JASMINEABI6199 <jasmineabi6199@gmail.com>
2024-03-11 05:30:19 -0700

    first commit

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://imatrix.com/wp-content/uploads/sites/12/2021/03/ecommerce.jpg" alt="Welcome picture" />
+    </div>
+  )
+}
+
